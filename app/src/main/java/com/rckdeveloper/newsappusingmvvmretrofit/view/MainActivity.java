8a715e6c3d37e9effb647068881ea75917fcda06
Abhishek Kumar <iabhishek5007@gmail.com>
2021-02-17 21:08:40 +0530

    Implemented View Binding

--- app/src/main/java/com/rckdeveloper/newsappusingmvvmretrofit/view/MainActivity.java
+++ app/src/main/java/com/rckdeveloper/newsappusingmvvmretrofit/view/MainActivity.java
@@ -3,14 +3,20 @@ package com.rckdeveloper.newsappusingmvvmretrofit.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.rckdeveloper.newsappusingmvvmretrofit.R;
+import com.rckdeveloper.newsappusingmvvmretrofit.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
     }
 }
